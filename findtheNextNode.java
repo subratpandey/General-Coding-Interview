2bf40c8ef77ce6a18daef5e5f6b57908e8c5e303
ggpaue <ggpaue@gmail.com>
2013-09-15 01:23:07 -0400

    findtheNextNode

---
+++ findtheNextNode.java
@@ -0,0 +1,32 @@
+
+public class Solution {
+	public TreeNode findNextLargest(TreeNode node) {
+		if(node == null) return null;
+		if(node.right != null) {
+			node = node.right;
+			while(node.left != null) {
+				node = node.left;
+			}
+			return node;	
+		}
+		
+		if(node.parent != null) {
+			TreeNode parent = node.parent;
+			if(parent.left.val == node.val) {
+				return parent;
+			} else if(parent.right.val == node.val) {
+				while(parent.parent != null) {
+					TreeNode prev = parent.parent;
+					if(prev.left.val == parent.val) {
+						break;
+					} else if(prev.right.val == parent.val) {
+						prev = parent.parent;
+					}
+				}
+				return parent.parent;
+			}
+		}
+		return null;
+	}
+
+}
