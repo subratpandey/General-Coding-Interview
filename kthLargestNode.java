5b35051a7e5f702680c24e3b63843b93c395b499
Guanpeng Gao <ggpaue@gmail.com>
2013-09-15 00:48:43 -0400

    Update kthLargestNode.java

--- kthLargestNode.java
+++ kthLargestNode.java
@@ -6,21 +6,31 @@
  */
 
 public class Solution {
-	public static TreeNode findKthLargest(TreeNode root, int k) {
-																	
+	static int counter = 0;
+	
+	public static TreeNode findKthLargest(TreeNode root, int k) {															
 		if(root == null) return null;
-									
-		int counter = 0;
+					
+		if(root != null) {
+			TreeNode right = findKthLargest(root.right, k);
+			counter++;
+			if (right != null) {
+				return right;
+		    }
 		
-		TreeNode temp = findKthLargest(root.right, k);
-		
-		
-		if(++counter == k) {
-			return root;
-		} else if (counter < k) {
-			temp = findKthLargest(root.left, k);
+			if(counter == k) {
+				return root;
+			} else if(counter < k) {
+				TreeNode left = findKthLargest(root.left, k);
+				if (left != null) {
+					return left;
+			    }
+			}
+			
 		}
-		return temp;
+		return null;
+		
 	}
+
 
 }
