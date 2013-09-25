2cfa433f3edf94d98ce24a096d8744c35f0d0343
ggpaue <ggpaue@gmail.com>
2013-09-24 22:49:35 -0400

    DecimaltoHexadecimal

---
+++ DecimaltoHexadecimal.java
@@ -0,0 +1,36 @@
+
+public class Solution {
+	private static final int sizeOfIntInHalfBytes = 8;
+	private static final int numberOfBitsInAHalfByte = 4;
+	private static final int halfByte = 0x0F;
+	private static final char[] hexDigits = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+	};
+	
+	public static String decToHex(int dec) {
+		StringBuilder hexBuilder = new StringBuilder(sizeOfIntInHalfBytes);
+		hexBuilder.setLength(sizeOfIntInHalfBytes);
+		for(int i = sizeOfIntInHalfBytes - 1; i >= 0; --i) {
+			int j = dec & halfByte;
+			hexBuilder.setCharAt(i, hexDigits[j]);
+			dec >>= numberOfBitsInAHalfByte;
+		}
+		return hexBuilder.toString();
+	}
+	
+	public static int convert(int n) {
+		  return Integer.valueOf(String.valueOf(n), 16);
+	}   // hexadecimal to decimal
+
+	
+	public static void main(String[] args) {
+		int dec = 32;
+		String hex = decToHex(dec);
+		System.out.println(hex);
+		System.out.println(convert(20));  // hexadecimal to decimal
+		System.out.println(convert(54));  // hexadecimal to decimal
+	}
+	
+
+}
