1ef3954142680610742a3d2d62c3044d407c3b9d
ggpaue <ggpaue@gmail.com>
2013-09-27 01:01:41 -0400

    remove duplicates in a string

---
+++ RemoveDupinString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Solution {
+	public static void main(String[] args) {
+		String string = "aabbccdefatafaz";
+		
+		char[] chars = string.toCharArray();
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		for(char c : chars) {
+			charSet.add(c);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(Character character : charSet) {
+			sb.append(character);
+		}
+		System.out.println(sb.toString());
+	}
+
+}
