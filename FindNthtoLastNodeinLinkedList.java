f9c071d383006f05f4101c82a94e0b7cf98d3c7c
ggpaue <ggpaue@gmail.com>
2013-10-02 01:04:11 -0400

    FindNthtoLastNodeinLinkedList

---
+++ FindNthtoLastNodeinLinkedList.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+	public ListNode nthToLast(ListNode head, int n) {
+		if(head == null || n < 1) return null;
+		ListNode p1 = head;
+		ListNode p2 = head;
+		
+		for(int i = 0; i < n - 1; ++i) {
+			if(p2 == null) return null;
+			p2 = p2.next;
+		}
+		
+		while(p2.next != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1;
+	}
+
+}
