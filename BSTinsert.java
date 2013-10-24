1ef671c8eb3cb0f24a4a555d7d4ef31652a5c46a
Guanpeng Gao <ggpaue@gmail.com>
2013-10-24 01:10:44 -0400

    Update BSTinsert.java

--- BSTinsert.java
+++ BSTinsert.java
@@ -1,24 +1,23 @@
-
 public class Solution {
 	public static TreeNode insertR(TreeNode root, int value) {
 		if(root == null) root = new TreeNode(value);
-		while(root != null) {
-			if(value < root.val) {
-				if(root.left == null) {
-					root.left = new TreeNode(value);
-				} else {
-					insertR(root.left, value);
-				}
-			} else if(value > root.val) {
-				if(root.right == null) {
-					root.right = new TreeNode(value);
-				} else {
-					insertR(root.right, value);
-				}
+		
+		if(value < root.val) {
+			if(root.left == null) {
+				root.left = new TreeNode(value);
 			} else {
-				throw new IllegalArgumentException("No Duplicates!");
+				insertR(root.left, value);
 			}
+		} else if(value > root.val) {
+			if(root.right == null) {
+				root.right = new TreeNode(value);
+			} else {
+				insertR(root.right, value);
+			}
+		} else {
+			throw new IllegalArgumentException("No Duplicates!");
 		}
+		
 		return root;
 	}
 	
