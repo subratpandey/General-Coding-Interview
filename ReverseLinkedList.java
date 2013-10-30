e0201ae678234a1ac0de0c94aa37fee3682051f2
ggpaue <ggpaue@gmail.com>
2013-10-30 00:06:14 -0400

    ReverseLinkedList
    Recursively and Iteratively

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,15 +1,28 @@
 
 public class Solution {
-	public ListNode reverse(ListNode head) {
+	public ListNode reverseR(ListNode head) {
 		if(head == null) return null;
 		if(head.next == null) return head;
 		
 		ListNode secondElem = head.next;
 		head.next = null;
-		ListNode reverseRest = reverse(secondElem);
+		ListNode reverseRest = reverseR(secondElem);
 		
 		secondElem.next = head;
 		return reverseRest;
+	}
+	
+	
+	public ListNode reverseN(ListNode head) {
+		ListNode temp;
+		ListNode prev = null;
+		while(head != null) {
+			temp = head.next;
+			head.next = prev;
+			prev = head;
+			head = temp;
+		}
+		return prev;
 	}
 
 }
