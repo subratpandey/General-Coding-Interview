5b22d43e46848e198d2eb01b36b716d9cb724e84
ggpaue <ggpaue@gmail.com>
2013-11-10 19:47:09 -0500

    ArrayHopper

--- ArrayHopper.java
+++ ArrayHopper.java
@@ -1,43 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-public class Solution {
-	public static ArrayList<Integer> minHops(int[] array) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		int l = 0, h = array.length;
-		
-		if(h == 0) return null;
-		if(array[l] == 0) return null;
-		
-		int x = array[l];
-		int max = 0;
-		int record = l;
-		int temp = 0;
-		result.add(record);
+import java.util.Scanner;
 
-		while(l + x < h) {
-			
-			for(int i = l + 1; i <= l + x; i++) {
-				if(array[i] == 0) continue;
-				temp = i + array[i];
-				if(temp >= max) {
+public class Solution {
+	public static void minHops(ArrayList<Integer> array) {
+
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		int l = 0, h = array.size();
+
+		if (h == 0 || array.get(0) == 0) {
+			System.out.println("fail");
+			return;
+		}
+
+		result.add(0);
+
+		while (l + array.get(l) < h) {
+			int record = -1;
+			int max = 0;
+			for (int i = l + 1; i <= l + array.get(l); i++) {
+				if (array.get(i) == 0)
+					continue;
+				int temp = i + array.get(i);
+				if (temp > max) {
 					max = temp;
 					record = i;
 				}
 			}
-			
+
+			if (record == -1 || array.get(record) == 0) {
+				System.out.println("fail");
+				return;
+			}
+
 			result.add(record);
 			l = record;
-			x = array[l];
 		}
-		
-		return result;
-	}
-	
-	public static void main(String[] args) {
-		int[] arr = {5, 6, 0, 4, 2, 4, 1, 0, 0, 4};
-		int[] arr2 = {0};
-		System.out.println(minHops(arr) + " out");
-		System.out.println(minHops(arr2) + " out");
-		
+		for (int i : result) {
+			System.out.print(i + ", ");
+		}
+		System.out.println("out");
 	}
 
+	public static void main(String[] args) {
+		String fileName = args[0];
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File(fileName));
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (line.length() == 0)
+					break;
+				int val = Integer.parseInt(line);
+				list.add(val);
+			}
+			minHops(list);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
